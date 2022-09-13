3d9b4082fd063976ac496404cb5d235ffce1b7d7
Timur287 <tmuxametov@gmail.com>
2022-09-13 12:57:36 +0300

    another new commit config

--- src/main/java/com/example/simplecrudapp/SimpleCrudAppApplication.java
+++ src/main/java/com/example/simplecrudapp/SimpleCrudAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class SimpleCrudAppApplication {
 
     public static void main(String[] args) {
